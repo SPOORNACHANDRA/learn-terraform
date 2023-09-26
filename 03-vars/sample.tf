e8b47bc5b2600115127ffe3f6c5def738c8a27f2
s poorna chandra <poornanaidu1@gmail.com>
2023-09-26 15:29:54 +0530

    terraform

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,62 +1,62 @@
-# plain variable
-variable "fruit_name" {
-  default = "apple"
-}
-
-output "fruit_name" {
-  value = var.fruit_name
-}
-
-# list variable
-
-variable "fruits" {
-  default = [
-  "apple",
-  "banana"]
-  //default =  [ "apple","banana"] //single line sntax
-}
-
-# map variable in plain
-variable "fruit_stock" {
-  default = {
-    apple = 100
-    banana = 200
-  }
-}
-
-# map variable, maps of maps
-variable "fruit_stock_with_price" {
-  default = {
-    apple = {
-      stock=100
-      price=3
-    }
-    banana={
-      stock=400
-      price=1
-    }
-  }
-}
-
-
-
-# how to access list variable, list index starts from zero
-output "fruits_first" {
-  value = var.fruits[0]
-}
-
-output "fruits_second" {
-  value = var.fruits[1]
-}
-
-# access a map variable
-output "fruit_stock_apple" {
-  value = var.fruit_stock["apple"]
-}
-# when ever we need apple stock
-output "fruit_stock_with_price_of_apple" {
-  value = var.fruit_stock_with_price["apple"].stock
-}
+## plain variable
+#variable "fruit_name" {
+#  default = "apple"
+#}
+#
+#output "fruit_name" {
+#  value = var.fruit_name
+#}
+#
+## list variable
+#
+#variable "fruits" {
+#  default = [
+#  "apple",
+#  "banana"]
+#  //default =  [ "apple","banana"] //single line sntax
+#}
+#
+## map variable in plain
+#variable "fruit_stock" {
+#  default = {
+#    apple = 100
+#    banana = 200
+#  }
+#}
+#
+## map variable, maps of maps
+#variable "fruit_stock_with_price" {
+#  default = {
+#    apple = {
+#      stock=100
+#      price=3
+#    }
+#    banana={
+#      stock=400
+#      price=1
+#    }
+#  }
+#}
+#
+#
+#
+## how to access list variable, list index starts from zero
+#output "fruits_first" {
+#  value = var.fruits[0]
+#}
+#
+#output "fruits_second" {
+#  value = var.fruits[1]
+#}
+#
+## access a map variable
+#output "fruit_stock_apple" {
+#  value = var.fruit_stock["apple"]
+#}
+## when ever we need apple stock
+#output "fruit_stock_with_price_of_apple" {
+#  value = var.fruit_stock_with_price["apple"].stock
+#}
 
 
 # variable datatypes
@@ -77,10 +77,19 @@ variable "fruit_details" {
 
   # variable ina acombination of any other string then it needs to be within ${}
 
+output "fruit_name_1" {
+  value = "fruitname = ${var.fruit_name}"
+}
+
+
+
   output "fruit_details_apple" {
     value = "apple_stock = ${var.fruit_details["apple"].stock}, apple_type = ${var.fruit_details["apple"].type}, apple_status = ${var.fruit_details["apple"].for_sale},"
   }
 
+output "fruit_details" {
+  value = "apple_stock = ${var.fruit_details.stock}, apple_type = ${var.fruit_details.type}, apple_status = ${var.fruit_details.for_sale},"
+}
 
 
 
