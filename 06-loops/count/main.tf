4b1401f3104721de3705a50e7c57c989a0eb8359
s poorna chandra <poornanaidu1@gmail.com>
2023-09-28 12:07:50 +0530

    terraform

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,16 @@
+variable "components" {
+  default = ["frontend", "mondodb"]
+}
+
+resource "aws_instance" "instance" {
+
+  count = length(var.components)
+
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0d8a6e86b202c9281" ]
+
+  tags = {
+    Name = ""
+  }
+}
