1213c4f569563a509b2d714685c2fae2222d2ed3
s poorna chandra <poornanaidu1@gmail.com>
2023-09-28 15:33:16 +0530

    terraform

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -1,12 +1,35 @@
-variable "components" {
+#variable "components" {
+#  default = {
+#    catalogue = { name = "catalogue_sg" }
+#    mongodb = { name = "mongodb_sg" }
+#  }
+#}
+#
+#
+#resource "aws_security_group" "allow_tls" {
+# for_each = var.components
+#  name = lookup(var.components,each.value["name"],null )
+#}
+#
+#
+
+variable "component" {
   default = {
-    catalogue = { name = "catalogue_sg" }
-    mongodb = { name = "mongodb_sg" }
+    cat = {
+      name = "catalogue_sg"
+    }
+    mon = {
+      name = "mongodb"
+    }
   }
 }
 
-
-resource "aws_security_group" "allow_tls" {
- for_each = var.components
-  name = lookup(var.components,each.value["name"],null )
+resource "aws_instance" "instances" {
+  for_each = var.component
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0d8a6e86b202c9281"]
+  tags                   = {
+    Name = lookup(var.component,each.key[""],null )
+  }
 }
