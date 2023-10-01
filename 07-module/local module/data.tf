068ebee71192cbc77c4f58bf1ec2d2ae379b2000
s poorna chandra <poornanaidu1@gmail.com>
2023-10-01 15:01:11 +0530

    terraform

--- 07-module/local module/data.tf
+++ 07-module/local module/data.tf
@@ -1,16 +1,17 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
+  most_recent = true
   name_regex = "centos-*"
   owners           = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami
 }
 
 
 
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami_ids.ami[0]
+  ami           = data.aws_ami[0]
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-0d8a6e86b202c9281" ]
 }
