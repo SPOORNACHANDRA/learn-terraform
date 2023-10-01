a4a4b4544acbc16da413bef9c23213b93527ed5c
s poorna chandra <poornanaidu1@gmail.com>
2023-10-01 15:01:44 +0530

    terraform

--- 07-module/local module/data.tf
+++ 07-module/local module/data.tf
@@ -5,13 +5,13 @@ data "aws_ami" "ami" {
 }
 
 output "ami" {
-  value = data.aws_ami
+  value = data.aws_ami.ami
 }
 
 
 
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami[0]
+  ami           = data.aws_ami.ami[0]
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-0d8a6e86b202c9281" ]
 }
