7dfd4954d74da4b89d9c9de3b212c20e7c5be686
s poorna chandra <poornanaidu1@gmail.com>
2023-10-01 16:07:58 +0530

    terraform

---
+++ roboshop-v4/module/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "instances" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.security_groups
+  tags                   = {
+    Name = var.name
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.name}-dev.poornadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instances.private_ip]
+}
+
+
+
+
+
