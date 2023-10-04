5a621c3daf281e1ebd2be1bbb217b12a17c62c2b
s poorna chandra <poornanaidu1@gmail.com>
2023-10-04 18:56:39 +0530

    terraform

--- roboshop-v5/main.tf
+++ roboshop-v5/main.tf
@@ -1,12 +1,12 @@
 module "components" {
 
-
+  for_each = var.components
 
   source          = "./module"
   zone_id         = var.zone_id
   security_groups = var.security_groups
-  name            = var.name
-  instance_type   = var.instance_type
+  name            = each.value["name"]
+  instance_type   = each.value["instance_type"]
 }
 
 
