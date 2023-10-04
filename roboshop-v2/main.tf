1ed52796b7961e2d51ecafd609d4a0224e3618e1
s poorna chandra <poornanaidu1@gmail.com>
2023-10-04 19:27:05 +0530

    terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -22,7 +22,9 @@ resource "aws_instance" "frontend" {
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.security_group
+  tags = {
     Name = "frontend"
+  }
   }
 
 resource "aws_route53_record" "frontend" {
