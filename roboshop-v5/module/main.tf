9d06db3898f14e52fb900ed4b77944974c067906
s poorna chandra <poornanaidu1@gmail.com>
2023-11-11 18:36:41 +0530

    terraform

--- roboshop-v5/module/main.tf
+++ roboshop-v5/module/main.tf
@@ -17,19 +17,18 @@ resource "aws_route53_record" "record" {
 }
 
 
-#
-#resource "null_resource" "ansible" {
-#
-#  depends_on = [
-#  aws_route53_record.record
-#  ]
-#  provisioner "local-exec" {
-#    command = <<EOF
-#cd /home/centos/ansible-roboshop-v1
-#git pull
-#sleep 30
-#ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
-#EOF
-#  }
-#}
+resource "null_resource" "ansible" {
 
+  depends_on = [
+  aws_route53_record.record
+  ]
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/ansible-roboshop-v1
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e
+ component= ${var.name}
+EOF
+  }
+}
