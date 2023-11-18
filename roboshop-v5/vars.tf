73466effde0e0814bb4fb8e86e53f06280cc139b
s poorna chandra <poornanaidu1@gmail.com>
2023-11-18 18:10:34 +0530

    terraform

--- roboshop-v5/vars.tf
+++ roboshop-v5/vars.tf
@@ -12,10 +12,10 @@ variable "components" {
       name= "frontend"
       instance_type= "t3.micro"
     }
-    mongodb={
-      name= "mongodb"
-      instance_type= "t3.micro"
-    }
+#    mongodb={
+#      name= "mongodb"
+#      instance_type= "t3.micro"
+#    }
 #    catalogue={
 #      name= "catalogue"
 #      instance_type= "t3.micro"
