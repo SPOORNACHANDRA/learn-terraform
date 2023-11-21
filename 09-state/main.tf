9729f37487e786899db0506eb63e569a1414bed4
s poorna chandra <poornanaidu1@gmail.com>
2023-11-21 13:41:34 +0530

    terraform

--- 09-state/main.tf
+++ 09-state/main.tf
@@ -1,17 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "practice-tf-state"
+    bucket = "poorna-tf-state"
     key    = "09-state/terraform.tfstate"
     region = "us-east-1"
   }
-}
-
-
-variable "test" {
-  default = "hello"
-}
-
-
-output "test" {
-  value = "var.test"
 }
