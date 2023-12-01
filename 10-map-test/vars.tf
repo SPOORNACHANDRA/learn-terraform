9e95fd34d6765dd08b8b96c97f2d184e51b95659
s poorna chandra <poornanaidu1@gmail.com>
2023-12-01 18:34:33 +0530

    terraform

--- 10-map-test/vars.tf
+++ 10-map-test/vars.tf
@@ -3,36 +3,23 @@ variable "vpc" {
     main = {
       cidr = "10.0.0.0/24"
       subnets = {
-        public1 = {
-          public = {cidr = "10.0.0.0/24", az = "us-east-1a"}
+        public = {
+          public1 = {cidr = "10.0.0.0/24", az = "us-east-1a"}
+          public2 = {cidr = "10.0.1.0/24", az = "us-east-1b"}
         }
-        app1 = {
-          app = {cidr = "10.0.2.0/24", az = "us-east-1a"}
+        app = {
+          app1 = {cidr = "10.0.2.0/24", az = "us-east-1a"}
+          app2 = {cidr = "10.0.3.0/24", az = "us-east-1b"}
         }
-        db1 = {
-          db = {cidr = "10.0.4.0/24", az = "us-east-1a"}
-        }
-        public2 = {
-          public = {cidr = "10.0.1.0/24", az = "us-east-1b"}
-        }
-        app2 = {
-          app = {cidr = "10.0.3.0/24", az = "us-east-1b"}
-        }
-        db2 = {
-          db = {cidr = "10.0.5.0/24", az = "us-east-1b"}
+        db = {
+          db1 = {cidr = "10.0.4.0/24", az = "us-east-1a"}
+          db2 = {cidr = "10.0.5.0/24", az = "us-east-1b"}
         }
       }
     }
   }
 
 }
-
-output "subnets" {
-  value = var.vpc
-}
-
-
-
 #locals {
 #  public = {for k,v in merge(var.vpc["main"]["subnets"]["public"]): k => v}
 #  app = {for k,v in merge(var.vpc["main"]["subnets"]["app"]): k => v}
