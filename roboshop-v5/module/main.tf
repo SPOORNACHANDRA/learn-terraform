3223f42038a96b97f4eb5a2b9340313e831720f6
s poorna chandra <poornanaidu1@gmail.com>
2023-12-29 14:20:35 +0530

    terraform

--- roboshop-v5/module/main.tf
+++ roboshop-v5/module/main.tf
@@ -26,7 +26,7 @@ resource "null_resource" "ansible" {
 cd /home/centos/ansible-roboshop
 git pull
 sleep 30
-ansible-playbook -i frontend-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 frontend.yml
+ansible-playbook -i ${var.name}-dev.poornadevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 
 EOF
   }
